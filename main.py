2f584dad570d631e4777823461766f8ccac30a01
Chefox <Chefoxee@yandex.ru>
2023-04-28 18:52:44 +0300

    Добавил ерунды

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 print("Оттачиваю git навык")
-input("Оттачиваешь навык? - ")
+
+
+while True:
+
+    question = input("Оттачиваешь навык? - ")
+
+    if question == "Да":
+        print("Тогда я создам словарь. Зачем? Потому что!")
+        dictionary = {"Рик":"Гениальный ученый", "Морти":"Ссыкливый внук"}
+        print(dictionary)
+        break
+    elif question == "Нет":
+        print("Ну и дурак")
+        break
+    else:
+        print("Либо Да либо Нет")
+
